9363bdcf1117a4161eca27a56bf65b9b6999bc47
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2016-12-09 15:40:29 +0100

    Started environments file testing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title = 'home works!';
+  connectionString = environment.data.defaultConnection.connectionString;
+
   constructor() {
   }
 
