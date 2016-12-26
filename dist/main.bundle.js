6c407c6037b4c38dc57cdb8fc8d3ed7eb19b18f9
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2016-12-26 17:52:00 +0100

    Aggiunte spunte

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -22356,11 +22356,15 @@ var RouterOutletMap = (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
 var environment = {
-    production: true,
-    envName: 'production',
+    production: false,
+    envName: 'development',
     bookStoreApi: {
-        server: 'http://talking-things-api.azurewebsites.net/',
+        server: 'http://localhost:5000/',
         apiUrl: 'api'
     }
 };
